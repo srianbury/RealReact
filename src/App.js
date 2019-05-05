88bdb2d73299c8712a2c8b9b8d49afb6b63b5e72
Brian Sunbury <bsunbury29@gmail.com>
2019-05-05 09:56:31 -0700

    homescreen
    added the description of this project to the homepage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {
 	HashRouter as Router,
 	Switch,
 	Route,
-	Link
+	NavLink
 } from 'react-router-dom';
 import './App.css';
 import { withCrud } from './CrudApp/Crud';
 import { UserInput, UserViewRow, userApiHandler } from './CrudApp/Deltas/Users';
 import { PhotoInput, PhotoViewRow, PhotoEdit, photoApiHandler } from './CrudApp/Deltas/Photos';
 
-const Home = () => { return(<div className='container'>Welcome!</div>); }
+
 const UsersWithCrud = withCrud(UserInput, UserViewRow, UserInput, userApiHandler);
 const PhotoswithCrud = withCrud(PhotoInput, PhotoViewRow, PhotoInput, photoApiHandler);
 const PhotosWithCrudWithDifferentEditForm = withCrud(PhotoInput, PhotoViewRow, PhotoEdit, photoApiHandler);
@@ -39,14 +39,62 @@ const App = () => {
 
 
 const Header = () => {
+	const linkStyle = 'mr-3 text-light';
+	const activeClassName = 'mr-3 text-info';
 	return(
-		<div className='container'>
-			<Link to="/" className='mr-1'>Home</Link>
-			<Link to="/users" className='mr-1'>Users</Link>
-			<Link to="/photos" className='mr-1'>Photos</Link>
-			<Link to="/photos2" className='mr-1'>Photos 2</Link>
+		<div className='p-2' style={{backgroundColor:'#515459'}}>
+			<div className='container'>
+				<NavLink to="/" className={linkStyle} activeClassName={activeClassName}>Home</NavLink>
+				<NavLink to="/users" className={linkStyle} activeClassName={activeClassName}>Users</NavLink>
+				<NavLink to="/photos" className={linkStyle} activeClassName={activeClassName}>Photos</NavLink>
+				<NavLink to="/photos2" className={linkStyle} activeClassName={activeClassName}>Photos 2</NavLink>
+			</div>
 		</div>
 	);
+}
+
+
+const Home = () => { 
+	const highlight = { backgroundColor: '#000000' };
+	return(
+		<div className='container'>
+			<div className="jumbotron mt-3">
+				<h1 className="display-3">Welcome!</h1>
+				<p className="lead">
+					The star of this show is <code>withCrud</code>!  It allows you to develop CRUD views quickly.  All you have to do is write 3 objects!  
+					<ol>
+						<li>Component for input</li>
+						<li>Component for viewing one row of data</li>
+						<li>An object that handles your api requests</li>
+					</ol>
+					withCrud maps your crud functions in your api handler to
+					the correct components, handles the toggling of editview, and handles the loading screen while the data is changing.
+				</p>
+			</div>
+			<div>
+				<p>
+					<h3>What's here?</h3>
+					These pages all use the same withEdit component to give you an example of its use.
+					withCrud allows you to develop CRUD views quickly because all you have to do is write 3 objects!  A component for input,
+					a component for viewing the data, and an object that handles your api requests!  Thanks to 
+					<a href='https://jsonplaceholder.typicode.com/'> JSONPlaceholder</a> I was able to write the crud operations
+					in <code>withCrud</code> as they would be for any other api so you don't need to reconfigure them.
+				</p>
+
+				<p>
+					<h3>Description of the pages</h3>
+					Users and Photos pull from different data sources but use the same withCrud component.
+					Those components look like this:
+					<div><code>const UsersWithCrud = withCrud(UserInput, UserViewRow, UserInput, userApiHandler);</code></div>
+					<div><code>const PhotoswithCrud = withCrud(PhotoInput, PhotoViewRow, PhotoInput, photoApiHandler);</code></div>
+
+					Photos and Photos2 are the same exept they use a different component for editing. i.e.
+					<div><code>const PhotoswithCrudSameForm = withCrud(PhotoInput, PhotoViewRow, <span style={highlight}>PhotoInput</span>, photoApiHandler);</code></div>
+					<div><code>const PhotosWithCrudDiffForm = withCrud(PhotoInput, PhotoViewRow, <span style={highlight}>PhotoEdit</span>, photoApiHandler);</code></div>
+				</p>
+			</div>
+		</div>
+	); 
 }
 
 
