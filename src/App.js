44d0edd4e1aa59670facd2f3f902dd141a5babfe
Brian Sunbury <bsunbury29@gmail.com>
2019-05-04 19:54:29 -0700

    added routes
    routes so you can see different view with different data using the same components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
+import {
+	HashRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
 import './App.css';
 import { withCrud } from './CrudApp/Crud';
 import { Input, ViewRow, basicApiHandler } from './CrudApp/Deltas/Basic';
 
 
-const UsersWithCrud = withCrud(Input, ViewRow, basicApiHandler);
+const BasicWithCrud = withCrud(Input, ViewRow, basicApiHandler);
+const Users = () => { return(<div>Temp</div>); }
 const App = () => {
 	return (
-		<UsersWithCrud />
+		<Router>
+			<Header />
+			<Switch>
+				<Route 
+					exact
+					component={BasicWithCrud}
+					path="/" />
+				<Route
+					component={Users}
+					path="/" />
+			</Switch>
+		</Router>
+	);
+}
+
+
+const Header = () => {
+	return(
+		<div className='container'>
+			<Link to="/" className='mr-1'>Home</Link>
+			<Link to="/users" className='mr-1'>Users</Link>
+		</div>
 	);
 }
 
 
 export default App;
 /*
-    You can easily port with with your own data assuming 
+    You can easily port with with your own data assuming
     your data is a list of object with a key value of id i.e then any other key-value-pairs
     data = [
         { id: 0, anythingElse: 'my value' },
